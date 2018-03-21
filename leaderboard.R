daa67e1b08358e1a70b946a732cd52a79ef73c74
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-03-21 22:01:11 +0100

    added some things

--- leaderboard.R
+++ leaderboard.R
@@ -1,2 +1,12 @@
+library(tidyverse)
 library(readr)
-read_csv("jigsaw-toxic-comment-classification-challenge-publicleaderboard.csv")
+submissions = read_csv("jigsaw-toxic-comment-classification-challenge-publicleaderboard.csv")
+
+head(submissions)
+
+lb <- submissions %>%
+    group_by(TeamName) %>%
+    summarize(score = max(Score)) %>%
+    arrange(desc(score))
+
+write_csv(lb, "leaderboard.csv")
